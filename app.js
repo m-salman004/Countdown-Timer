7b0daa4026e790aa1f7ab9f3f9824afc08a88e0a
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-04 08:13:27 -0800

    setInterval() and clearInterval()

--- app.js
+++ app.js
@@ -63,8 +63,27 @@ const months = [
     let hours = Math.floor((t % oneDay) / oneHour);
     let minutes = Math.floor((t % oneHour) / oneMinute);
     let seconds = Math.floor((t % oneMinute) / 1000);
+    // set values array
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
   
-
+    items.forEach(function (item, index) {
+      item.innerHTML = format(values[index]);
+    });
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+      }
+    }
+    // countdown;
+    let countdown = setInterval(getRemaindingTime, 1000);
+    //set initial values
+    getRemaindingTime();
 
 
 
